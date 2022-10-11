77d42ea0574a6d071699a42a07215b1535383d69
Raul Gallegos Duarte <raulgallegosduarte@MacBook-Air-de-Raul.local>
2022-10-11 11:56:21 +0200

    initial commit: level 0

---
+++ src/films.js
@@ -0,0 +1,50 @@
+// Exercise 1: Get the array of all directors.
+function getAllDirectors() {
+
+}
+
+// Exercise 2: Get the films of a certain director
+function getMoviesFromDirector() {
+
+}
+
+// Exercise 3: Calculate the average of the films of a given director.
+function moviesAverageOfDirector() {
+
+}
+
+// Exercise 5: Order by year, ascending
+function orderByYear() {
+
+}
+
+// Exercise 6: Calculate the average of the movies in a category
+function moviesAverageByCategory() {
+
+}
+
+// Exercise 7: Modify the duration of movies to minutes
+function hoursToMinutes() {
+
+}
+
+// Exercise 8: Get the best film of a year
+function bestFilmOfYear() {
+
+}
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = {
+    getAllDirectors,
+    getMoviesFromDirector,
+    moviesAverageOfDirector,
+    orderAlphabetically,
+    orderByYear,
+    moviesAverageByCategory,
+    hoursToMinutes,
+    bestFilmOfYear,
+  };
+}
